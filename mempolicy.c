265756a005745e0f4bd995f060e64737589c0f7d
Sirajus Salekin <ssalek14@earlham.edu>
2017-12-14 16:34:44 -0500

    Update mempolicy.c

--- mempolicy.c
+++ mempolicy.c
@@ -5,14 +5,6 @@
 #include "mproc.h"
 #include "memheader.c"
 
-#define FIRST_FIT 0
-#define NEXT_FIT 1
-#define BEST_FIT 2
-#define WORST_FIT 3
-#define RANDOM_FIT 4
-
-int CUSTOM_MEM_POLICY = FIRST_FIT;
-
 int do_cyclemempolicy(void){
     CUSTOM_MEM_POLICY = (CUSTOM_MEM_POLICY + 1) % 5;
 
