596c20205552ef225fcdb12b729d5ce55da715fe
Sirajus Salekin <ssalek14@earlham.edu>
2017-12-14 18:47:41 -0500

    Update memStat.c

--- memStat.c
+++ memStat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "/usr/src/minix/servers/is/inc.h"
+#include <minix/sysinfo.h>
 //#include "/usr/src/minix/servers/pm/misc.c"
 #include "/usr/src/minix/lib/libsys/getsysinfo.c"
 #include "/usr/src/minix/servers/pm/mproc.h"
