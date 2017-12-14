7ec26b009f9a1765c9c9e37a58539ff129f34a5f
Sirajus Salekin <ssalek14@earlham.edu>
2017-12-14 17:09:26 -0500

    Update testMemAlloc.c

--- testMemAlloc.c
+++ testMemAlloc.c
@@ -3,7 +3,8 @@
 #include <sys/syscall.h>
 
 int main(){
+  message m;
   printf("Calling PM_CYCLEMEMPLOICY using system calls\n");
-  _syscall(PM_PROC_NR, 48);
+  _syscall(PM_PROC_NR, 48, &m);
 return 0;
 }
