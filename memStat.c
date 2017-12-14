5b9620b10fa93788c34670599dd957ecde629627
Sirajus Salekin <ssalek14@earlham.edu>
2017-12-14 17:37:04 -0500

    Create memStat.c

---
+++ memStat.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+int main(){
+    struct pm_mem_info pmi;
+    int i;
+
+    getsysinfo(PM_PROC_NR, SI_MEM_ALLOC, &pmi);
+    for (i = 0; i < _NR_HOLES; i++){
+        printf("Hole size: %d", pmi.pmi_holes[i].h_len);
+        }
+
+return 0;
+}
